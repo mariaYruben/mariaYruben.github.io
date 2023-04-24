4f0f1e06c61c3f7bea837496415dd375cc77e186
Luis Parra <e.aszendit08.tid@telefonica.com>
2023-04-24 10:51:32 +0200

    Updates

---
+++ precache-manifest.3fc7576e7fcfa735dc7bbe5ae86c30ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c01b72927176e47543a5",
+    "url": "/static/css/main.e68846d1.chunk.css"
+  },
+  {
+    "revision": "c01b72927176e47543a5",
+    "url": "/static/js/main.c01b7292.chunk.js"
+  },
+  {
+    "revision": "bf318286bf0f1184ace9",
+    "url": "/static/js/1.bf318286.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "32f079eff165e7e2f38ae187d5083d10",
+    "url": "/index.html"
+  }
+];
